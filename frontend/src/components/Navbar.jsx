d2c3ae66c2dbdbd9a16ceb813563f15717874334
Tariqul911 <zxtariqul911@gmail.com>
2026-05-13 15:45:45 +0800

    feat: initial commit - navbar, banner UI and express server setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between bg-white p-5">
+      <ul className="flex gap-3 font-medium text-xl">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/destinations"}>Destinations</Link>
+        </li>
+        <li>
+          <Link href={"/my-bookings"}>My Bookings</Link>
+        </li>
+      </ul>
+      <div>
+        <Image
+          alt="wanderlast-logo"
+          src={"/assets/Wanderlast.png"}
+          width={150}
+          height={150}
+        ></Image>
+      </div>
+      <ul className="flex gap-3 font-medium text-xl">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link href={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link href={"sign-up"}>Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
